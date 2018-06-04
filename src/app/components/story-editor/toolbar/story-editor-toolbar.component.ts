deba6641830d45264daffe5bd69c98f04c759b15
Måns Isaksson <mans.95.isaksson@gmail.com>
2018-06-04 21:41:16 +0200

    Toolbar formatting

--- src/app/components/story-editor/toolbar/story-editor-toolbar.component.ts
+++ src/app/components/story-editor/toolbar/story-editor-toolbar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-story-editor-toolbar',
-  templateUrl: './story-editor-toolbar.component.html'
+  templateUrl: './story-editor-toolbar.component.html',
+  styleUrls: ['./story-editor-toolbar.component.css']
 })
 export class StoryEditorToolbarComponent implements OnInit {
 
