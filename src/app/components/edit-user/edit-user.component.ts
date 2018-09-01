1498bf989300ccb080e8f602f44b104394faa681
Måns Isaksson <mans.95.isaksson@gmail.com>
2018-09-01 22:03:57 +0200

    Edit user form rework

--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../_models';
-import { AuthenticationService } from '../../_services';
+import { Component, OnInit } from '@angular/core'
+import { User } from '../../_models'
+import { AuthenticationService } from '../../_services'
+import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms'
 
 @Component({
   selector: 'edit-user',
@@ -8,20 +9,53 @@ import { AuthenticationService } from '../../_services';
   styleUrls: ['./edit-user.component.css']
 })
 export class EditUserComponent implements OnInit {
+  private registerForm: FormGroup
+  private submitted = false
   private user: User = new User()
 
-  constructor(private authService: AuthenticationService) {
+  constructor(private authService: AuthenticationService, private formBuilder: FormBuilder) {
 
   }
-  
+
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: EditUserComponent.MatchPassword
+    })
+
     this.authService.getCurrentUser().subscribe(user => {
       this.user = user;
+      if (!this.user) {
+        this.user = new User()
+      }
     })
   }
 
-  getCurrentUserId() {
+  static MatchPassword(AC: AbstractControl) {
+    let password = AC.get('password').value
+    let confirmPassword = AC.get('confirmPassword').value
+    if (password != confirmPassword) {
+      AC.get('confirmPassword').setErrors({ MatchPassword: true })
+    } else {
+      return null
+    }
+  }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls }
+
+  onSubmit() {
+    this.submitted = true
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return
+    }
+
+    alert('SUCCESS!! :-)')
   }
 
 }
