a44159d8dd75b8d7a68f58b6c91cf7b7b84d4ba8
Måns Isaksson <mans.95.isaksson@gmail.com>
2018-09-08 22:58:17 +0200

    Replaced quill renderer

--- src/app/components/story-viewer/story-viewer.component.ts
+++ src/app/components/story-viewer/story-viewer.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
 import { StoryService, AlertService } from '../../_services/index'
 import { ChapterContent, StoryMetaData, ChapterMetaData } from '../../_models/index'
-import QuillDeltaToHtmlConverter = require('quill-delta-to-html');
+
+import * as Quill from 'quill'
 
 @Component({
   selector: 'app-story-viewer',
@@ -10,18 +11,19 @@ import QuillDeltaToHtmlConverter = require('quill-delta-to-html');
   styleUrls: ['./story-viewer.component.css']
 })
 export class StoryViewerComponent implements OnInit {
+  private tempStory = <StoryMetaData>{
+    title: "...",
+    chapters: []
+  }
+
   private storyId: string
-  private story: StoryMetaData
+  private story: StoryMetaData = this.tempStory
 
   constructor(
     private storyService: StoryService,
     private alertService: AlertService,
     private activatedRoute: ActivatedRoute
-  ) {
-    this.story = <StoryMetaData>{
-      title: "..."
-    }
-  }
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params) => {
@@ -34,23 +36,32 @@ export class StoryViewerComponent implements OnInit {
     this.storyService.getStory(this.storyId).then((story) => {
       this.story = story
       this.storyService.setCurrentlyViewedStory(story)
-      let chapterURIs = story.chapters.map(a => a.URI)
-      this.storyService.getChapterContents(chapterURIs).then((contents: ChapterContent[]) => {
-        let storyHTML = ""
-        let cfg = {}
-        story.chapters.forEach((chapter: ChapterMetaData) => {
-          storyHTML += '<div class="storyChapter">'
-          storyHTML += '<h2>' + chapter.title + '</h2>'
-          try {
-            let content = contents.find(a => { return chapter.URI == a.URI })
-            let deltaObject = JSON.parse(content.content)
-            let converter = new QuillDeltaToHtmlConverter(deltaObject.ops, cfg)
-            storyHTML += converter.convert()
-          } catch {}
-          storyHTML += '</div>'
-        })
-        document.getElementById('documentContainer').innerHTML = storyHTML
-      }).catch((e) => console.log(e))
+
+      if (!this.story) {
+        this.story = this.tempStory
+      }
+
+      setTimeout(() => { // One frame delay to let the html update
+        let chapterURIs = story.chapters.map(a => a.URI)
+        this.storyService.getChapterContents(chapterURIs).then((contents: ChapterContent[]) => {
+          var options = {
+            modules: {
+              toolbar: ''
+            },
+            placeholder: 'Nothing here yet...',
+            readOnly: true,
+            theme: 'bubble'
+          }
+          story.chapters.forEach((chapter: ChapterMetaData) => {
+            try {
+              var editor = new Quill('#' + 'quill_content_' + chapter.chapterId, options)
+              let content = contents.find(a => { return chapter.URI == a.URI })
+              editor.setContents(JSON.parse(content.content))
+            } catch { }
+          })
+  
+        }).catch((e) => console.log(e))
+      }, 0)
     }).catch((e) => console.log(e))
   }
 
